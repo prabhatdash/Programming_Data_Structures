edfab1d40dde4590b6680c95261c7e8ec55e2a51
Prabhat Das <prabhatdash0376@gmail.com>
2023-03-23 16:14:25 +0530

    Add files via upload

---
+++ selection_sort.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+int arr[]={5,1,10,4,3};
+int temp,min_idx;
+int size=sizeof(arr)/sizeof(int);
+for(int i=0;i<size-1;i++)
+{
+    min_idx = i;
+    for(int j=i+1;j<size;j++){
+        if(arr[j] < arr[min_idx]){
+            min_idx = j;
+        }
+    }
+    if(min_idx != i)
+    {
+        temp=arr[min_idx];
+        arr[min_idx]=arr[i];
+        arr[i]=temp;
+    }
+}
+for(int i=0;i<size;i++){
+    printf("%d ",arr[i]);
+}
+}
