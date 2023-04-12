5ca4807e294025cc2b3245c03e80c980b3f93e91
Prabhat <prabhatdash0376@gmail.com>
2023-04-12 10:08:06 +0530

    Update shell_sort.c

--- 1. Array Operations/5. Sorting/shell_sort.c
+++ 1. Array Operations/5. Sorting/shell_sort.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
-
 int main() {
   int arr[] = {10,9,6,4,1,7,5};
   int size = sizeof(arr) / sizeof(arr[0]);
-  
-  for (int gap = size / 2; gap > 0; gap=gap/2) {
-    for (int i = gap; i < size; i++) {
-      int temp = arr[i];
-      for (int j = i; j>= gap && arr[j - gap] > temp; j=j-gap) {
-        arr[j] = arr[j - gap];
+  int i,j,gap,temp;
+  for (gap=size/2; gap>0; gap=gap/2) {
+    for (i=gap; i<size; i++) {
+       temp=arr[i];
+      for (j=i; j>= gap && arr[j-gap]>temp; j=j-gap) {
+        arr[j]=arr[j-gap];
       }
-      arr[j] = temp;
+      arr[j]=temp;
     }
   }
 printf("Sorted arr: \n");
- for (int i = 0; i < size;i++) {
-    printf("%d  ", arr[i]);
+ for (i=0;i<size;i++) {
+    printf("%d  ",arr[i]);
   }
 }
