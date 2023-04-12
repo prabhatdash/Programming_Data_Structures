ec1b112aef6119c8e9d9ba98df2ba779ffc165b4
Prabhat <prabhatdash0376@gmail.com>
2023-04-12 10:03:53 +0530

    Update shell_sort.c

--- 1. Array Operations/5. Sorting/shell_sort.c
+++ 1. Array Operations/5. Sorting/shell_sort.c
@@ -7,8 +7,7 @@ int main() {
   for (int gap = size / 2; gap > 0; gap=gap/2) {
     for (int i = gap; i < size; i++) {
       int temp = arr[i];
-      int j;
-      for (j = i; j >= gap && arr[j - gap] > temp; j=j-gap) {
+      for (int j = i; j>= gap && arr[j - gap] > temp; j=j-gap) {
         arr[j] = arr[j - gap];
       }
       arr[j] = temp;
