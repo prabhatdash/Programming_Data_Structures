39c13a3048429b8ec43cc8e58e95de0354a4e2ca
Prabhat Das <prabhatdash0376@gmail.com>
2023-05-04 10:32:10 +0530

    Create queue_operation.c

---
+++ 4. Queue/queue_operation.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+int main()
+{
+    int front=0,rear=0,i,j=1,size,ch;
+    printf("ENTER THE SIZE OF QUEUE");
+    scanf("%d",&size);
+    int queue[size];
+    printf("\n1.Insertion \n2.Deletion \n3.Display \n4.Exit");
+    while(1)
+    {
+        printf("\nEnter the Choice:");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+        case 1:
+            if(rear==size)
+                printf("\n Queue is Full");
+            else
+            {
+                printf("\n Enter no %d:",j++);
+                scanf("%d",&queue[rear++]);
+            }
+            break;
+        case 2:
+            if(front==rear)
+            {
+                printf("\n Queue is empty");
+            }
+            else
+            {
+                printf("\n Deleted Element is %d",queue[front++]);
+                size++;
+            }
+            break;
+        case 3:
+            printf("\nQueue Elements are:\n ");
+            if(front==rear)
+                printf("\n Queue is Empty");
+            else
+            {
+                for(i=front; i<rear; i++)
+                {
+                    printf("%d",queue[i]);
+                    printf("\n");
+                }
+                break;
+            case 4:
+                exit(0);
+            default:
+                printf("Wrong Choice: please see the options");
+            }
+        }
+    }
+}
